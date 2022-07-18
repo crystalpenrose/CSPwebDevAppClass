bcfae2d009edeb9c1bf81011f5d4f1fba766f22d
crystalpenrose <crystal@penrose.us>
2022-07-18 16:20:08 -0700

    more changes

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-console.log("Hello world")
+console.log("ContactCapture")
 
-let introField = document.querySelector(".intro-field")
+let nameField = document.querySelector(".name-field")
+let emailField = document.querySelector(".email-field")
+let phoneField = document.querySelector(".phone-field")
 let button = document.querySelector("button")
 
 button.onclick = function(){
-    let intro = introField.value
-    let message = "I can't believe you said " + intro
-    console.log(message)
-    document.write(message)
+    let name = nameField.value
+    let message1 = "Your name address is " + name
+    let email = emailField.value
+    let message2 = "Your email address is " + email
+    let phone = phoneField.value
+    let message3 = "Your phone address is " + phone
+    let message4 = "You are now in Therelene's Boogie-Boarding Bluegrass Band Fan Club"
+    console.log(message1, message2, message3)
+    document.write(message1 + "<br/>", message2 + "<br/>", message3 + "<br/>", message4)
 }
